ae5c4ce5cab0ab7c1c519dc7c24ff8e6121c1671
gmleach14 <66041219+gmleach14@users.noreply.github.com>
2020-10-31 10:14:40 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ var chartGroup = svg.append("g")
 d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
 // Import Data
-d3.csv("assets/data/data.csv", function(err, healthData) {
-  if (err) throw err;
-console.log(healthData)
+d3.csv("assets/data/data.csv").then(function(healthData) {
+  // if (err) throw err;
+  console.log(healthData)
   // Step 1: Parse Data/Cast as numbers
   healthData.forEach(function(data) {
     data.poverty = +data.poverty;
